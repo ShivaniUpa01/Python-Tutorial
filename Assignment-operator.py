59f05bc4c8cd41655734361cc7267c1aacc113f7
ShivaniUpa01 <108825975+ShivaniUpa01@users.noreply.github.com>
2022-07-16 14:12:36 +0530

    Examples of Assignment Operators
    Output : 
    
    
    Assign :  10
    Add & Assign :  20
    Subtract & Assign :  10
    Multiply & Assign :  100
    Bitwise l-swift and assign :  102400

---
+++ Assignment-operator.py
@@ -0,0 +1,22 @@
+#
+a = 10
+
+# Assign value
+b = a
+print("Assign : ",b)
+
+# Add and assign value
+b += a
+print("Add & Assign : " b)
+
+# Subtract and assign value
+b -= a
+print("Subtract & Assign : ",b)
+
+# multiply and assign
+b *= a
+print("Multiply & Assign : ",b)
+
+# bitwise lishift operator
+b <<= a
+print("Bitwise l-swift and assign : ",b)
