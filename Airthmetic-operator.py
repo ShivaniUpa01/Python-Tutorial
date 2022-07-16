6b3d18f35b84d9a2988bd9b644e0266f3303a41a
ShivaniUpa01 <108825975+ShivaniUpa01@users.noreply.github.com>
2022-07-16 12:59:42 +0530

    Examples of Arithmetic Operator
    Output :
    
    Add :  13
    Subtract :  5
    Multiplication :  36
    Division :  2.25
    Division 2nd (Floor) :  2
    Modular :  1
    Power :  6561

---
+++ Airthmetic-operator.py
@@ -0,0 +1,32 @@
+a = 9
+b = 4
+
+# Addition of numbers
+add = a + b
+
+# Subtraction of numbers
+sub = a - b
+
+# Multiplication of number
+mul = a * b
+
+# Division(float) of number
+div1 = a / b
+
+# Division(floor) of number
+div2 = a // b
+
+# Modulo of both number
+mod = a % b
+
+# Power
+p = a ** b
+
+# print results
+print("Add : ",add)
+print("Subtract : ", sub)
+print("Multiplication : ",mul)
+print("Division : ", div1)
+print("Division 2nd (Floor) : ",  div2)
+print("Modular : ",mod)
+print("Power : ",p)
