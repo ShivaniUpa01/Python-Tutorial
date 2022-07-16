e2592df86663c1e6a300a643970c096dee3410f3
ShivaniUpa01 <108825975+ShivaniUpa01@users.noreply.github.com>
2022-07-16 14:00:59 +0530

    Examples of Bitwise operators
    Output : 
    
    Bitwise AND :  0
    Bitwise OR :  14
    Bitwise NOT :  -11
    Bitwise XOR :  14
    Bitwise Right Swift :  2
    Bitwise Left Swift :  40
    
    Because its working on bits(Binary number 0 and 1) that's why it's result is different

---
+++ Bitwise-operator.py
@@ -0,0 +1,20 @@
+a = 10
+b = 4
+
+# Print bitwise AND operation
+print("Bitwise AND : ",a & b)
+
+# Print bitwise OR operation
+print("Bitwise OR : ",a | b)
+
+# Print bitwise NOT operation
+print("Bitwise NOT : ",~a)
+
+# print bitwise XOR operation
+print("Bitwise XOR : ",a ^ b)
+
+# print bitwise right shift operation
+print("Bitwise Right Swift : ",a >> 2)
+
+# print bitwise left shift operation
+print("Bitwise Left Swift : ",a << 2)
