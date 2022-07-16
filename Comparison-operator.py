0544537efb4172ba4a739a4ba3c4abe8e7b4366b
ShivaniUpa01 <108825975+ShivaniUpa01@users.noreply.github.com>
2022-07-16 13:29:12 +0530

    Examples of Relational Operators
    Output : 
    
    Greter than : False
    Less Than :  True
    Equals To :  False
     Not Equals To :  True
    Greater Than Equals To :  False
    Less Than Equals To :  True

---
+++ Comparison-operator.py
@@ -0,0 +1,20 @@
+a = 13
+b = 33
+
+# a > b is False
+print("Greter than :",a > b)
+
+# a < b is True
+print("Less Than : ",a < b)
+
+# a == b is False
+print("Equals To : ",a == b)
+
+# a != b is True
+print(" Not Equals To : ",a != b)
+
+# a >= b is False
+print("Greater Than Equals To : ",a >= b)
+
+# a <= b is True
+print("Less Than Equals To : ",a <= b)
