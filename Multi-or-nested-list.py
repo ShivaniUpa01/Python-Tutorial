da0b5e7619ee9822f98b0431ce6dc3eb4ad4544e
ShivaniUpa01 <108825975+ShivaniUpa01@users.noreply.github.com>
2022-07-19 21:28:05 +0530

    Create and fetch nested(multi dimensional) list
    Output : 
    Accessing a element from a Multi-Dimensional list
    For
    Geeks

---
+++ Multi-or-nested-list.py
@@ -0,0 +1,8 @@
+List = [['Geeks', 'For'], ['Geeks']]
+  
+# accessing an element from the
+# Multi-Dimensional List using
+# index number
+print("Accessing a element from a Multi-Dimensional list")
+print(List[0][1])
+print(List[1][0])
