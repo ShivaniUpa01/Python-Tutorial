b76c75685393b89f82acad1a20931b1724daa140
ShivaniUpa01 <108825975+ShivaniUpa01@users.noreply.github.com>
2022-07-19 21:26:00 +0530

    Delete Reserve-listt.py

--- Reserve-listt.py
+++
@@ -1,9 +0,0 @@
-List = [1, 2, 'Geeks', 4, 'For', 6, 'Geeks']
-
-# accessing an element using
-# negative indexing
-print("Accessing element using negative indexing")
-
-# print the last element of list
-for i in range(1,len(List)+1):
-  print(List[-i], end=" ")
